9c2a724022b0716085c5e679d67c1a8fcebd18a6
veitsi <xtfkpi@gmail.com>
2015-11-14 01:16:20 +0200

    it launches glider

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var N=5;
+var N=7;
 var field=[];
+var cursor=false;
 initModelField();
-field[0][1][1]=true;
-field[0][2][2]=true;
+field[0][1][2]=true;
+field[0][2][3]=true;
+field[0][3][1]=true;
+field[0][3][2]=true;
+field[0][3][3]=true;
 console.log(field[0]);
 initHtmlField();
+
+function neighboursNumber(i,j){
+    return 0+field[+cursor][i-1][j-1]+field[+cursor][i-1][j]+field[+cursor][i-1][j+1]+
+        field[+cursor][i][j-1]+field[+cursor][i][j+1]+
+        field[+cursor][i+1][j-1]+field[+cursor][i+1][j]+field[+cursor][i+1][j+1];
+}
 
 function initModelField(){
     field[0]=[];
@@ -19,24 +29,9 @@ function initModelField(){
         }
     }
 }
-//<div id="game-field">
-//    <div class="row">
-//    <div></div><div></div><div></div><div></div><div></div>
-//</div>
-//<div class="row">
-//    <div></div><div class="alive"></div><div></div><div class="alive"></div><div></div>
-//</div>
-//<div class="row">
-//    <div></div><div class="alive"></div><div></div><div></div><div></div>
-//</div>
-//<div class="row">
-//    <div></div><div></div><div></div><div></div><div></div>
-//</div>
-//<div class="row">
-//    <div></div><div></div><div></div><div></div><div></div>
-//</div>
-//</div>
+
 function initHtmlField(){
+    document.write('<input value="recalc" onclick="recalc()" type="button"></input>');
     document.write('<div id="game-field">')
     for (var i=1;i<N+1;i++){
         document.write('<div class="row">')
@@ -51,12 +46,19 @@ function initHtmlField(){
 
 }
 
-function renderField(num){
-    num=num||0;
+function recalc(){
     for (var i=1;i<N+1;i++){
         for (var j=1;j<N+1; j++){
+            var n=neighboursNumber(i,j);
 
+            field[+!cursor][i][j]=false;
+            //console.log(i,j,n,field[+cursor][i][j]);
+            document.body.childNodes[3].childNodes[i-1].childNodes[j-1].className='';
+            if (field[+cursor][i][j] && (n===2 || n===3)) field[+!cursor][i][j]=true;
+            if ((!field[+cursor][i][j]) && n===3) field[+!cursor][i][j]=true;
+            if (field[+!cursor][i][j])
+                document.body.childNodes[3].childNodes[i-1].childNodes[j-1].className='alive';
         }
     }
-
+    cursor=!cursor;
 }
